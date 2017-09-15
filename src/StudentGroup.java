962b7907c3280ed3d4aa8e0f00666ec88ecf53eb
Shruthi-Nadigoti <shruthi.nadigoti@gmail.com>
2017-09-16 00:47:31 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -237,7 +237,7 @@ public class StudentGroup implements StudentArrayOperation {
 					}
 				}
 			}
-		int Students[] newarr=new Student[count];
+		Students[] newarr=new Student[count];
 		int j=0;
 		for(int i=0; i< students.length;i++)
 		{
@@ -286,7 +286,7 @@ public class StudentGroup implements StudentArrayOperation {
 				count++;
 			}
 		}
-		int Student[] newarr=new Student[count];
+		Student[] newarr=new Student[count];
 		int j=0;
 		for(int i=0; i< students.length;i++)
 		{
