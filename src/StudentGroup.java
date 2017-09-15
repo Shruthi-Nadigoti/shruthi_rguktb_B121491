7c186427205993128175d26499ed96cdf54c2003
Shruthi-Nadigoti <shruthi.nadigoti@gmail.com>
2017-09-16 01:18:04 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -252,7 +252,13 @@ public class StudentGroup implements StudentArrayOperation {
 		// Add your implementation here
 		return null;
 	}
-
+	public static int getDifferenceDays(Date d1, Date d2) {
+			int daysdiff=0;
+			long diff = d2.getTime() - d1.getTime();
+			long diffDays = diff / (24 * 60 * 60 * 1000)+1;
+			 daysdiff = (int) diffDays;
+			return daysdiff;
+ 	}
 	@Override
 	public int getCurrentAgeByDate(int indexOfStudent) throws IllegalArgumentException{
 		// Add your implementation here
@@ -266,6 +272,12 @@ public class StudentGroup implements StudentArrayOperation {
 		LocalDate now = new LocalDate();
 		Years age = Years.yearsBetween(birthdate, now);
 		return age.getYears();*/
+		if(indexOfStudent==0)
+			throw new IllegalArgumentException();
+		else
+		{
+			return getDifferenceDays(this.students[indexOfStudent].getBirthDate(),new Date())/365;
+		}
 		return 0;
 		
 	}
